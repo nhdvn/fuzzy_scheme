a47f8702e225d4d4c7076c266a8ef40d7caad2c1
Potato <18120019@student.hcmus.edu.vn>
2022-05-28 00:21:27 +0700

    fit 1 byte

--- 3.0/CryptoSystem.py
+++ 3.0/CryptoSystem.py
@@ -5,46 +5,46 @@ import FuzzyScheme
 
 class VoiceCryptoSystem:
 
-    extractor = FuzzyScheme.SampleLock(0.2, 256)
-    ivstorage = {}
+    def __init__(self, size):
+        
+        self.template_size = size
+        self.reliable_size = size >> 1
+        self.extractor = FuzzyScheme.SampleLock(0.2, size >> 1)
+        self.storage = {}
 
 
-    def __init__(self) -> None:
-        pass
+    def enroll_user(self, uid: int, vdata: list):
 
-
-    def enroll_user(self, uid: int, sample: list):
-
-        index = self.select_reliable_index(sample)
-        self.ivstorage[uid] = index
-        input = self.extract_reliable_bits(sample, index)
+        index = self.select_reliable_index(vdata)
+        self.storage[uid] = index
+        input = self.extract_reliable_bits(vdata, index)
         return self.extractor.key_generate(uid, input)
 
 
-    def verify_user(self, uid: int, sample: list):
+    def verify_user(self, uid: int, vdata: list):
 
-        index = self.ivstorage[uid]
-        input = self.extract_reliable_bits(sample, index)
+        index = self.storage[uid]
+        input = self.extract_reliable_bits(vdata, index)
         return self.extractor.key_reproduce(uid, input)
 
 
-    def select_reliable_index(self, data: numpy.ndarray):
+    def select_reliable_index(self, vdata: numpy.ndarray):
 
         result = []
 
-        for i in range(512):
-            mean = data[:, i].mean()
-            diff = data[:, i] - mean
-            sqrs = (diff ** 2).sum()
-            result.append(sqrs)
+        for i in range(self.template_size):
+            mean = vdata[:, i].mean()
+            dist = vdata[:, i] - mean
+            vari = (dist ** 2).sum()
+            result.append(vari)
 
-        return numpy.argsort(result)[:256]
+        return numpy.argsort(result)[:self.reliable_size]
 
 
-    def extract_reliable_bits(data: numpy.ndarray, index: list):
+    def extract_reliable_bits(self, vdata: numpy.ndarray, index: list):
 
         res = ''
-        mean = data.mean(axis = 0)
+        mean = vdata.mean(axis = 0)
 
         for val in mean[index]:
             res += '0' if val <= 0 else '1'
--- 3.0/FuzzyScheme.py
+++ 3.0/FuzzyScheme.py
@@ -1,36 +1,33 @@
 
 import math
-import random
 import hashlib
-import secrets
+import os
+import random
 
 
 class DigitalLocker:
 
-    N = None # 28 bytes - 224 bits hash
-    K = None # 16 bytes - 128 bits secret
-    R = None # 18 bytes - 144 bits nonce
-
-
     def __init__(self, _N, _K, _R):
-        self.N = _N
-        self.K = _K
-        self.R = _R
+        
+        self.N = _N # 28 bytes - 224 bits hash
+        self.K = _K # 16 bytes - 128 bits secret
+        self.R = _R # 18 bytes - 144 bits nonce
+        
         self.P = bytes(self.N - self.K)
 
 
     def xor(self, b1: bytes, b2: bytes) -> bytes:
 
-        val = int.from_bytes(b1, 'big') ^ int.from_bytes(b2, 'big')
+        val = int.from_bytes(b1, "big") ^ int.from_bytes(b2, "big")
 
-        return val.to_bytes(self.N, 'big')
+        return val.to_bytes(self.N, "big")
 
 
     def lock(self, key: bytes, value: bytes) -> bytes:
 
-        nonce = secrets.token_bytes(self.R)
+        nonce = os.urandom(self.R)
 
-        vhash = hashlib.new('SHA224', nonce + key).digest()
+        vhash = hashlib.sha224(nonce + key).digest()
 
         return nonce + self.xor(vhash, value + self.P)
 
@@ -39,7 +36,7 @@ class DigitalLocker:
 
         nonce, vlock = data[:self.R], data[self.R:]
 
-        vhash = hashlib.new('SHA224', nonce + key).digest()
+        vhash = hashlib.sha224(nonce + key).digest()
 
         vlock = self.xor(vhash, vlock)
 
@@ -49,62 +46,65 @@ class DigitalLocker:
 
 class SampleLock:
 
-    locker = DigitalLocker(28, 16, 18)
-    nbytes = 16
-    strlen = 80
+    def __init__(self, rate, size):
 
-
-    def __init__(self, rate, size) -> None:
-        self.bound = math.floor(math.exp(rate * self.strlen))
+        self.locker = DigitalLocker(28, 16, 18)
+        self.key_size = 16
+        self.sub_size = 80
+        self.pub_size = 126 # = 80 + 46
+        
+        self.bound = math.floor(math.exp(rate * self.sub_size))
         self.range = range(size)
 
 
-    def bit_to_bytes(self, bio: str, idx: list):
+    def bits_to_bytes(self, bio: str, idx: list):
 
-        return int(str.join('', [bio[i] for i in idx]), 2).to_bytes(10, 'big')
+        subset = "".join([bio[i] for i in idx])
 
-
-    def arr_to_bytes(self, bio: str, idx: list):
-
-        idx_bytes = bytes.join(b'', [i.to_bytes(2, 'big') for i in idx])
-
-        return idx_bytes, self.bit_to_bytes(bio, idx)
+        return int(subset, 2).to_bytes(10, "big")
 
 
-    def arr_from_bytes(self, bio: str, arr: bytes):
+    def array_to_bytes(self, idx: list, bio: str):
 
-        idx = [int.from_bytes(arr[i: i + 2], 'big') for i in range(0, 160, 2)]
-
-        return self.bit_to_bytes(bio, idx)
+        return bytes(idx), self.bits_to_bytes(bio, idx)
 
 
-    def key_generate(self, uid: int, bio: str):
+    def public_to_bytes(self, arr: bytes, bio: str):
 
-        secret = secrets.token_bytes(self.nbytes)
-        helper = open(f'./Helper_{uid}', 'wb')
+        return self.bits_to_bytes(bio, [i for i in arr])
+
+
+    def random_subset(self):
+
+        return random.sample(self.range, self.sub_size)
+
+
+    def key_generate(self, ID: int, bio: str):
+
+        secret = os.urandom(self.key_size)
+        helper = open(f"./Helper_{ID}", "wb")
 
         for _ in range(self.bound):
 
-            idx = random.sample(self.range, self.strlen)
-            idx, sub = self.arr_to_bytes(bio, idx)
-            buffer = idx + self.locker.lock(sub, secret)
-            helper.write(buffer)
+            idx = self.random_subset()
+            idx, sub = self.array_to_bytes(idx, bio)
+            vlocked = self.locker.lock(sub, secret)
+            helper.write(idx + vlocked)
 
         helper.close()
         return secret
 
 
-    def key_reproduce(self, uid: int, bio: str):
+    def key_reproduce(self, ID: int, bio: str):
 
-        helper = open(f'./Helper_{uid}', 'rb')
+        helper = open(f"./Helper_{ID}", "rb")
 
-        for _ in range(self.bound):
-
-            block = helper.read(206)
-            idx, val = block[:160], block[160:]
-            sub = self.arr_from_bytes(bio, idx)
+        while block := helper.read(self.pub_size):
+            
+            idx = block[:self.sub_size]
+            val = block[self.sub_size:]
+            sub = self.public_to_bytes(idx, bio)
             secret = self.locker.unlock(sub, val)
-
             if secret != None: break
 
         helper.close()
